e4fa179031cece7c2fe1caf48767a32600205e17
Arif <to.areeb@gmail.com>
2017-09-10 14:50:25 -0400

    extend class using prototype

--- ES5_Class.js
+++ ES5_Class.js
@@ -18,3 +18,15 @@ console.log(obj.public_var);
 console.log(obj.pubfun(5,3));
 console.log(obj.getPrivateVar);
 console.log(obj.getPubFun(5,3));
+
+var calc = function(){
+    this.add=function(a,b){
+        return a+b;
+    };    
+};
+calc.prototype.subt= function(a,b){
+    return a-b;
+}
+var objCalc = new calc();
+console.log("Addition "+objCalc.add(5,3));
+console.log("subtraction "+objCalc.subt(5,3));
