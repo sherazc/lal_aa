37b1efec4cbdf3619e15535896dee8bee1d080e8
DESKTOP-4O54AIV\Quazi <to.areeb@gmail.com>
2017-09-30 23:00:20 -0400

    Template Literal prac

---
+++ lal_104_es6/TemplateLiteral.js
@@ -0,0 +1,10 @@
+var First = "Mohammad";
+let Middle="Arif";
+const Last="Quazi";
+
+let FullName=`${First} ${Middle.substr(0,2)} ${Last}`;
+
+console.log(FullName);
+console.log(`this is test 
+for multipe line 
+with backtick`);
