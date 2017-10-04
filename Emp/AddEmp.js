9a4f9acfa5d3898eb60b330ea1b6ca2f8378a4dd
DESKTOP-4O54AIV\Quazi <to.areeb@gmail.com>
2017-10-04 12:00:46 -0400

    connection and insert successful to my sql

--- Emp/AddEmp.js
+++ Emp/AddEmp.js
@@ -1,10 +1,10 @@
 let mysql = require("mysql");
 let conString = {
     host: "localhost",    
-    user:"sa",
+    user:"arif",
     password:"abc123",
-    port:8889,
-    database:"emp"
+    port:3306,
+    database:"shop"
 };
 
 let con= mysql.createConnection(conString);
@@ -16,6 +16,16 @@ con.connect((error)=>{
     else{
         console.log("Successful");
     }
+});
+
+let queryObject ={
+    Name:"Quazi",
+    salary:100
+}
+let query = con.query("INSERT INTO emp set ?",queryObject,function(error,result){
+    console.log("insret query", query.sql);
+    console.log("insert id",result. insertId);
+    console.log("Ending..");
 });
 
 con.end((error)=>{
