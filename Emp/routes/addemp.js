1d4b4278571a412e818885926601051d1c8183c5
DESKTOP-4O54AIV\Quazi <to.areeb@gmail.com>
2017-10-05 20:20:47 -0400

    completed with insert and select

--- Emp/routes/addemp.js
+++ Emp/routes/addemp.js
@@ -1,50 +1,61 @@
 var express = require("express");
 var router = express.Router();
-
+var bodyparser= require("body-parser");
+var http = require('http');
+// var app = express();
+// app.use(bodyparser());
 /* GET users listing. */
 router.post('/', function(req, res, next) {
-   
-   let mysql = require("mysql");
-    let conString = {
-        host: "localhost",    
-        user:"arif",
-        password:"abc123",
-        port:3306,
-        database:"shop"
-    };
+    var strconcat="";
+let mysql = require("mysql");
+let conString = {
+    host: "localhost",    
+    user:"arif",
+    password:"abc123",
+    port:3306,
+    database:"shop"
+};
 
-    let con= mysql.createConnection(conString);
-    con.connect((error)=>{
-        if(error){
-           // res.json({"Error" :true, "Message":"Error while opening connection"});
-            //res.write(error.fatal);
-        }
-        else{
-            // res.write('Successful');
-            //res.json({"Error": "true", "Message": "Successfully connected"});
-        }
-    });
-    //res.json({"test": "test", "Message": "Successfully connected"});
-    let queryObject ={
-        Name:"test",
-        salary:100
+let con= mysql.createConnection(conString);
+con.connect();
+//res.json({"test": "test", "Message": "Successfully connected"});
+let queryObject ={
+    Name: req.body.empname,
+    salary:req.body.salary
+}
+let query = con.query("INSERT INTO emp set ?",queryObject,function(error,result){
+    console.log("Insert query ",query.sql);
+    console.log(result.insertId);
+    if(error) {
+       console.log(error);
+    } else {
+        console.log("User Added !");
+        strconcat = strconcat.concat(`${queryObject.Name} added successfully!<br />`);
     }
-    let query = con.query("INSERT INTO emp set ?",queryObject,function(error,result){
-        if(error) {
-            res.json({"Error" : true, "Message" : "Error executing MySQL query"});
-        } else {
-            res.json({"Error" : false, "Message" : "User Added !"});
-        }
-    });
-
-    con.end((error)=>{
-        if(error){
-            res.json({"connection disconnect Error":"true"});
-        }
-        else{
-            res.json({"Connection close.":"close"});
-        }
-    });
-    //res.send('respond with a resource employee');
 });
+
+let querySel = con.query("select * from emp order by id",function(error,result){
+    console.log("select query ",querySel.sql);
+    strconcat=strconcat.concat('<table border="1">');
+    var tempcon="";
+    result.forEach(record=>{
+        tempcon = `<tr>
+        <td>${record.id}</td>
+        <td>${record.Name}</td>
+        <td>${record.salary}</td>
+        </tr>`;
+        strconcat =strconcat.concat(tempcon);
+        console.log(`emp name ${record.Name} : salary ${record.salary}`);
+    });
+    strconcat =strconcat.concat("</table>");
+    console.log(strconcat);
+    req.body.emplist = strconcat;
+    res.send(strconcat);
+});
+con.end();
+
+});
+
+
+  
 module.exports = router;
