1f5029febd546b2d5b75a592e6a5ef818e4895ea
Sheraz <sheraz@mbp.com>
2018-02-15 08:17:51 -0500

    Spring annotation jdbc

---
+++ src/lal_110_spring-boot/sc05_spring_annotation_jdbc/src/main/java/com/lal/SpringConfigs.java
@@ -0,0 +1,35 @@
+package com.lal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("com.lal")
+public class SpringConfigs {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public DataSource dataSource() {
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        EmbeddedDatabase db = builder
+                .setType(EmbeddedDatabaseType.HSQL)
+                .addScript("classpath:create_db.sql")
+                .build();
+        return db;
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate jdbcTemplate() {
+        return new NamedParameterJdbcTemplate(dataSource);
+    }
+}
